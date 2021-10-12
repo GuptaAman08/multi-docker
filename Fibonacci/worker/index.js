0008a5f7cda2163913b6d685b959022d0e3323e9
Aman Gupta <2015aman.a@ves.ac.in>
2021-10-12 22:58:58 +0530

    Changing worker process logic a bit

--- Fibonacci/worker/index.js
+++ Fibonacci/worker/index.js
@@ -1,27 +1,19 @@
-const redis = require('redis');
 const keys = require('./keys');
+const redis = require('redis');
 
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
 });
-
-// Creating duplicate redis connection instance since once the connection is put on subscribtion mode
-// we can run only redis commands related to subcruption only.
 const sub = redisClient.duplicate();
 
-
-function fib(num){
-    if (num < 2){
-        return 1
-    }
-    return fib(num-1) + fib(num-2)
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
 sub.on('message', (channel, message) => {
-    console.log("New Value Inserted into REDIS")
-    console.log("Channel Name", channel)
-    redisClient.hset("hvalues", message, fib(parseInt(message)))
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
-
 sub.subscribe('insert');
